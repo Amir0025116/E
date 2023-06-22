8f0d092520fbb2b794afade1e48966958e3a03c4
Amir <133496947+Amir0025116@users.noreply.github.com>
2023-06-22 18:31:21 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 const burger = document.querySelector('.burger'),
-    links = document.querySelector('.header-nav-links')
-
+    links = document.querySelector('.header-nav-links'),
+    body = document.querySelector('body')
 burger.addEventListener('click', function () {
     this.classList.toggle('active')
     links.classList.toggle('open')
+    body.classList.toggle('bodyActive')
 })
  
+function scrollTo(element) {
+    window.scroll({
+        left: 0,
+        top: element.offsetTop,
+        behavior:'smooth'
+    })
+}
+var button = document.querySelector('.buttonScroll')
+var scrolling = document.querySelector('.sub')
+
+button.addEventListener('click', () => {
+    scrollTo(scrolling);
+})
